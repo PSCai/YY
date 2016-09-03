823f71e252f2772cd0b2416554162fdf048e2c5b
1030665496@qq.com <psc@smile.local>
2016-09-03 17:26:28 +0800

    初始化项目

---
+++ YY/YY/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YY
+//
+//  Created by psc on 16/9/3.
+//  Copyright © 2016年 PSCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YY/YY/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YY
+//
+//  Created by psc on 16/9/3.
+//  Copyright © 2016年 PSCai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
